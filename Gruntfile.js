723a6e078519e54c9062ba40dc49fcc5e7931cfe
antyang <mrant0ny@gmail.com>
2015-09-14 12:22:29 -0700

    remove duplicate /grunfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,27 +373,27 @@ module.exports = function (grunt) {
       }
     },
 
-    buildcontrol: {
-      options: {
-        dir: 'dist',
-        commit: true,
-        push: true,
-        connectCommits: false,
-        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-      },
-      heroku: {
-        options: {
-          remote: 'heroku',
-          branch: 'master'
-        }
-      },
-      openshift: {
-        options: {
-          remote: 'openshift',
-          branch: 'master'
-        }
-      }
-    },
+    // buildcontrol: {
+    //   options: {
+    //     dir: 'dist',
+    //     commit: true,
+    //     push: true,
+    //     connectCommits: false,
+    //     message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+    //   },
+    //   heroku: {
+    //     options: {
+    //       remote: 'heroku',
+    //       branch: 'master'
+    //     }
+    //   },
+    //   openshift: {
+    //     options: {
+    //       remote: 'openshift',
+    //       branch: 'master'
+    //     }
+    //   }
+    // },
 
     // Run some tasks in parallel to speed up the build process
     concurrent: {
